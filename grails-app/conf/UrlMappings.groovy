23d171e083ebb5d9a4e8984092f7528c61e56f4e
cDor <christoph.dornieden@gmail.com>
2014-07-11 14:24:43 +0200

    revert mapping

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -2,34 +2,11 @@ class UrlMappings {
 
 	static mappings = {
 
-        group("/intern") {
-            "/shift/$action?/$id?(.$format)?"(controller: 'Shift')
-            "/employee/$action?/$id?(.$format)?"(controller: 'Employee')
-        }
-
-        group("/employee"){
-            "/index/$id?(.$format)?" (redirect:"/intern/employee/index")
-            "/create/$id?(.$format)?" (redirect:"/intern/employee/create")
-            "/edit/$id?(.$format)?" (redirect:"/intern/employee/edit")
-            "/show/$id?(.$format)?" (redirect:"/intern/employee/show")
-        }
-
-        group("/shift"){
-            "/index/$id?(.$format)?" (redirect:"/intern/shift/index")
-            "/create/$id?(.$format)?" (redirect:"/intern/shift/create")
-            "/edit/$id?(.$format)?" (redirect:"/intern/shift/edit")
-            "/show/$id?(.$format)?" (redirect:"/intern/shift/show")
-        }
-
-
-
-
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
         }
-
 
 
         "/"(view:"/index")
